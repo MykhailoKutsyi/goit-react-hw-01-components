68897453a73800eb67b2c5bc67c1d6b316956b40
Mykhailo Kutsyi <91030243+MykhailoKutsyi@users.noreply.github.com>
2022-03-20 22:12:12 +0200

    updateAFewMoments

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import Profile from './components/Profile/Profile'
 import user from './components/Profile/user.json'
 
-import FriendList from './components/FriendList/FriendListItem'
-import friends from "./components/FriendList/friends.json";
-
 import Statistics from './components/Statistics/Statistics'
 import data from './components/Statistics/data.json'
+
+import FriendList from './components/FriendList/FriendListItem'
+import friends from "./components/FriendList/friends.json";
 
 import TransactionHistory from './components/TransactionHistory/TransactionHistory'
 import transactions from './components/TransactionHistory/transactions.json'
@@ -22,11 +22,10 @@ function App() {
           stats={user.stats}
       />
 
-      <FriendList
-        friends={friends} />
-      
       <Statistics title="Upload stats" stats={data} />
       <Statistics stats={data} />
+
+      <FriendList friends={friends} />
 
       <TransactionHistory items={transactions} />;
     </div>
--- src/components/Statistics/Statistics.js
+++ src/components/Statistics/Statistics.js
@@ -3,36 +3,16 @@ import Stats from './Stats'
 import s from './Statistics.module.css'
 
 const Statistics = ({ title, stats }) => {
-    if (title !== undefined) {
-        return (
-            <section className={s.statistics}>
-                <h2 className={s.title}>{title}</h2>
-                <ul className={s.statList}>
-                    {stats.map(item => (
+    return (
+        <section className={s.statistics}>
+            {title && <h2 className={s.title}>{title}</h2>}
+            <ul className={s.statList}>
+                {stats.map(item => (
                     <Stats
                         key={item.id}
                         label={item.label}
                         percentage={item.percentage}
                     />
-                    ))}
-                </ul>
-            </section>
-        );
-    }
-    // (title ?? (() => {
-    //     console.log(title);
-    //     (<h2 className="title">Upload stats</h2>)
-    // }));
-    // (title ?? 4);
-    return (
-        <section className={s.statistics}>
-            <ul className={s.statList}>
-                {stats.map(item => (
-                <Stats
-                    key={item.id}
-                    label={item.label}
-                    percentage={item.percentage}
-                />
                 ))}
             </ul>
         </section>
@@ -40,6 +20,7 @@ const Statistics = ({ title, stats }) => {
 }
 
 Statistics.propTypes = {
+    title: PropTypes.string,
     stats: PropTypes.array.isRequired,
 };
 
